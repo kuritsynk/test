3407d6e65668439aedaa354aa14013cc8b0d96cc
Konstantin Kuritsyn <kuritsyn@mail.ru>
2019-07-26 10:36:08 +0300

    #1 Запрашивать имя пользователя и выводить его на экран в строке приветствия

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <string>
+
+std::string ask_user_name() {
+	std::cout << "Enter user name:";
+	std::string user_name;
+	std::cin >> user_name;
+	return user_name;
+}
 
 int main() {
-	std::cout << "Hello user" << std::endl;
+	std::cout << "Hello, " << ask_user_name() << "!" << std::endl;
 }
